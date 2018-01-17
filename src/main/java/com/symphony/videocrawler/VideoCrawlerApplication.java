1d1d75005f23bc7ff8daeae4bb405aca802c85c6
郑忠 <529931497@qq.com>
2018-01-17 10:57:22 +0800

    网络视频爬虫系统初始化

---
+++ src/main/java/com/symphony/videocrawler/VideoCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.symphony.videocrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCrawlerApplication.class, args);
+	}
+}
